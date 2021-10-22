c1900b020be1afe44929c3016bc07aeaeb7f60b0
ceejaytalinio <christianjoy.talinio@gmail.com>
2021-10-22 16:45:11 +0800

    first commit gitbash

---
+++ AssignmentOne/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AssignmentOne
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hi I'm Christian Joy Talinio :)" , "It's nice to meet you");
+        }
+    }
+}
